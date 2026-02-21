e1bb2ee85bd3577197c2fa0bd66f1d37bc37852b
SantiagoAmaya21 <santiago.amaya-z@mail.escuelaing.edu.co>
2026-02-21 18:51:06 -0500

    web + chunking + vector store

---
+++ ingest.py
@@ -0,0 +1,35 @@
+from bs4.filter import SoupStrainer
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+def build_vector_store(embeddings):
+
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs={
+            "parse_only": SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        },
+    )
+
+    docs = loader.load()
+    print(f"[+] Loaded {len(docs)} docs")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    splits = text_splitter.split_documents(docs)
+    print(f"[+] Split into {len(splits)} chunks")
+
+    vector_store = Chroma.from_documents(
+        splits,
+        embedding=embeddings
+    )
+
+    print("[+] Vector store created")
+
+    return vector_store
